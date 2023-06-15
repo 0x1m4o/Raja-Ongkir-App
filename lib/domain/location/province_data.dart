81d0337ec390bf72153f82cb3eb85ebb88a335d3
0x1m4o <chuntdevnx@gmail.com>
2023-06-15 20:42:18 +0700

    Set up API models

---
+++ lib/domain/location/province_data.dart
@@ -0,0 +1,36 @@
+// This file is "main.dart"
+// ignore_for_file: invalid_annotation_target
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'province_data.freezed.dart';
+part 'province_data.g.dart';
+
+// Result and Status Section
+@freezed
+abstract class ProvinceData with _$ProvinceData {
+  // Response
+  factory ProvinceData.provinceDetailData(
+          @JsonKey(name: "province_id") String provinceID, String province) =
+      ProvinceDetailData;
+
+  // Status
+  factory ProvinceData.statusResponse(int code, String description) =
+      ProvinceStatusResponse;
+
+  factory ProvinceData.fromJson(Map<String, dynamic> json) =>
+      _$ProvinceDataFromJson(json);
+}
+
+// All Response section
+@freezed
+abstract class ProvinceResponse with _$ProvinceResponse {
+  // Query, Status and Result
+  factory ProvinceResponse.provinceResponseDetail(
+      List<dynamic> query,
+      ProvinceStatusResponse status,
+      List<ProvinceDetailData> result) = ProvinceDetailResponse;
+
+  factory ProvinceResponse.fromJson(Map<String, dynamic> json) =>
+      _$ProvinceResponseFromJson(json);
+}
---
+++ lib/domain/location/province_data.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'province_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ProvinceDetailData _$$ProvinceDetailDataFromJson(Map<String, dynamic> json) =>
+    _$ProvinceDetailData(
+      json['province_id'] as String,
+      json['province'] as String,
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$ProvinceDetailDataToJson(
+        _$ProvinceDetailData instance) =>
+    <String, dynamic>{
+      'province_id': instance.provinceID,
+      'province': instance.province,
+      'runtimeType': instance.$type,
+    };
+
+_$ProvinceStatusResponse _$$ProvinceStatusResponseFromJson(
+        Map<String, dynamic> json) =>
+    _$ProvinceStatusResponse(
+      json['code'] as int,
+      json['description'] as String,
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$ProvinceStatusResponseToJson(
+        _$ProvinceStatusResponse instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'description': instance.description,
+      'runtimeType': instance.$type,
+    };
+
+_$ProvinceDetailResponse _$$ProvinceDetailResponseFromJson(
+        Map<String, dynamic> json) =>
+    _$ProvinceDetailResponse(
+      json['query'] as List<dynamic>,
+      ProvinceStatusResponse.fromJson(json['status'] as Map<String, dynamic>),
+      (json['result'] as List<dynamic>)
+          .map((e) => ProvinceDetailData.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$ProvinceDetailResponseToJson(
+        _$ProvinceDetailResponse instance) =>
+    <String, dynamic>{
+      'query': instance.query,
+      'status': instance.status,
+      'result': instance.result,
+    };
